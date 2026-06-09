9aa5579e8aec42d1776815ed6293a0c19c47cadf
gustavin002 <gustavomaia957@gmail.com>
2026-06-09 17:29:41 -0300

    feito o template editais

--- src/main/java/com/bidding/front/bidding/front/controller/EditalPageController.java
+++ src/main/java/com/bidding/front/bidding/front/controller/EditalPageController.java
@@ -4,12 +4,23 @@
  */
 package com.bidding.front.bidding.front.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/editais")
 public class EditalPageController {
+    
+    @GetMapping("/list")
+    public String listar(HttpSession session){
+        Object token = session.getAttribute("token");
+        
+        if(token == null) {
+           return "redirect:/login";
+        }
+        return "editais";
+    }
 
 }
